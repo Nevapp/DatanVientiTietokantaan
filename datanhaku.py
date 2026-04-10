7d4b347a838a4cc58de39c1e6e54e49b5a26312b
Nevapp <pepneval@edu.lapinamk.fi>
2026-04-10 03:33:16 +0300

    Update datanhaku.py
    copypastattu korjattu versio vs codesta

--- datanhaku.py
+++ datanhaku.py
@@ -14,7 +14,7 @@ def get_bbox(city):
     response = requests.get(osm_url, headers=headers).json()
 
     if not response:
-        return None
+     return None
 
     osm_bbox = response[0]['boundingbox']
     min_lat, max_lat, min_lon, max_lon = osm_bbox
@@ -24,55 +24,70 @@ def get_bbox(city):
 
 
 def get_openaq_locations_by_bbox(_bbox):
+
     response = requests.get(
-        f'https://api.openaq.org/v3/locations?limit=1000&page=1&order_by=id&sort_order=asc&bbox={_bbox}',
-        headers={'X-API-Key': API_KEY})
+     f'https://api.openaq.org/v3/locations?limit=1000&page=1&order_by=id&sort_order=asc&bbox={_bbox}',
+     headers={'X-API-Key': API_KEY})
 
     if response.status_code == 200:
         return response.json()['results']
-    else:
-        print("Virhe:", response.status_code)
-        return []
-
+    
+    return []
 
 def download_file_by_location(location_id, year, month, day):
-    date_str = f"{year}{month:02d}{day:02d}"
-    base_url = "https://openaq-data-archive.s3.amazonaws.com"
-    key = f"records/csv.gz/locationid={location_id}/year={year}/month={month:02d}/location-{location_id}-{date_str}.csv.gz"
-    full_url = f"{base_url}/{key}"
+ date_str = f"{year}{month:02d}{day:02d}"
+ base_url = "https://openaq-data-archive.s3.amazonaws.com"
+   
+ key = f"records/csv.gz/locationid={location_id}/year={year}/month={month:02d}/location-{location_id}-{date_str}.csv.gz"
+ full_url = f"{base_url}/{key}"
 
-    response = requests.get(full_url)
+ response = requests.get(full_url)
 
-    if response.status_code == 200:
-        df = pd.read_csv(io.BytesIO(response.content), compression='gzip')
+ if response.status_code != 200:
+    print("Ei dataa:")
+    return
+ df = pd.read_csv(io.BytesIO(response.content), compression='gzip')
+ 
+ conn=get_connection()
+ cursor=conn.cursor()
 
-conn=get_connection()
-cursor=conn.cursor()
-
-for _, row in df.iterrows():
-    cursor.execute("""
-    INSERT INTO Mittaukset (sensoriID, arvo, aika)
-    VALUES (%s,%s,%s)
-    """,(row ["sensoriId"],row ["arvo"],row ["aika"]))
-    conn.commit()
-    cursor.close()
-    conn.close()
+ for _, row in df.iterrows():
+         sensor_id = row["sensors_id"]
+         cursor.execute("""
+         INSERT IGNORE INTO sensori (id, parametri, yksikko, mittauspaikkaID)
+         VALUES (%s,%s,%s,%s)
+         """,(
+             sensor_id,
+             row["parameter"],
+             row["units"],
+             location_id
+          ))
+         cursor.execute("""
+                        
+         INSERT INTO mittaukset (sensoriID, arvo, aika)
+         VALUES (%s,%s,%s)
+         """,(
+                            sensor_id,
+                            row["value"],
+                            row["datetime"]
+                        ))
+ conn.commit()
+ cursor.close()
+ conn.close()
         
-    print("Tallennettu:", f"{location_id}-{date_str}.csv")
-else:
-    print("Ei dataa, status:", response.status_code)
+ print("Tallennettu:", date_str)
+ 
 
 
 if __name__ == "__main__":
-    bbox = get_bbox("New York City")
-    print("BBOX:", bbox)
+ bbox = get_bbox("Helsinki")
+print("BBOX:", bbox)
 
-    locations = get_openaq_locations_by_bbox(bbox)
-    print("Locations löytyi:", len(locations))
+locations = get_openaq_locations_by_bbox(bbox)
+print("Locations löytyi:", len(locations))
 
-    if locations:
-        first_location_id = locations[0]["id"]
-        print("Haetaan data location_id:", first_location_id)
-        download_file_by_location(first_location_id, 2023, 1, 15)
+if locations:
+        location_id = locations[0]["id"]
         for day in range(1, 31):
-            download_file_by_locations(first_location_id, 2023, 1, day)
+            download_file_by_location(location_id, 2023, 1, day)
+
